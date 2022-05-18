d3bbb4d85a8af2798bad14aa3a81087f2b9e5533
Arthur Keen <aakeen@yahoo.com>
2022-05-17 18:35:00 -0600

    modified test.py file to test both target variants.  added a metadata.ttl file containing rdf/rdfs/owl/skos metamodels

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 from arangoSemantics import ArangoSemantics
 
 
-kg = ArangoSemantics("http://0.0.0.0:8529", "root", "password", "ontologyTest", "arangoSemantic")
+kg = ArangoSemantics("http://localhost:8529", "root", "openSesame",   "ontologyTest", "arangoSemanticsRDF")
 
-# kg.init_rdf_collections(bnode="Blank")
-# kg.import_rdf("iao.owl")
+kg.init_rdf_collections(bnode="Blank")
+kg.import_rdf("iao.owl")
 
-kg.init_ontology_collections()
-
-#kg.import_ontology("test.owl", format="turtle")
-kg.import_ontology("iao.owl")
+kg2 = ArangoSemantics("http://localhost:8529", "root", "openSesame",   "ontologyTest", "arangoSemanticsOWL")
+kg2.init_ontology_collections()
+kg2.import_ontology("test.owl", format="turtle")
+#kg2.import_ontology("iao.owl")
