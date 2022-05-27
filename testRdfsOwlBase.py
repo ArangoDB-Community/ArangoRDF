cee7f67f0343631def9cdbffa9d9686ced0b8952
Arthur Keen <aakeen@yahoo.com>
2022-05-27 17:04:38 -0600

    separate rdfowl and skos in metadata folder and tested

--- testRdfsOwlBase.py
+++ testRdfsOwlBase.py
@@ -4,8 +4,8 @@ from arangoSemantics import ArangoSemantics
 # ToDo use metamodel files for RDFS, OWL, and SKOS that can optionally be loaded
 kg = ArangoSemantics("http://localhost:8529", "root", "openSesame",   "ontologyTest", "arangoSemanticsRDF")
 kg.init_rdf_collections(bnode="Blank")
-kg.import_rdf("metamodel.ttl")
-
+kg.import_rdf("./metadata/rdfowl.ttl")
+kg.import_rdf("./metadata/skos.ttl")
 # kg2 = ArangoSemantics("http://localhost:8529", "root", "openSesame",   "ontologyTest", "arangoSemanticsOWL")
 # kg2.init_ontology_collections()
 # #kg2.import_ontology("test.owl", format="turtle")
