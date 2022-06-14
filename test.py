6b57edc7859be8d387cd0e655e89120b66776313
David Vidovich <dvidovich@missionsolutionsgroup.com>
2022-06-14 09:10:23 -0400

    cleaned up test.py

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 from arangoSemantics import ArangoSemantics
 
-
-kg = ArangoSemantics("http://0.0.0.0:8529", "root", "password", "import5", "arangoSemantic")
-
 #rdf import
+kg = ArangoSemantics("http://0.0.0.0:8529", "root", "password", "rdf", "arangoSemantic")
 kg.init_rdf_collections(bnode="Blank")
 kg.import_rdf("testExport3.owl", format="turtle")
 
 #ontology import
-# kg.init_ontology_collections()
-#kg.import_ontology("iao.owl")
+kg2 = ArangoSemantics("http://0.0.0.0:8529", "root", "password", "ontologyTest", "arangoSemantic")
+kg2.init_ontology_collections()
+kg2.import_ontology("iao.owl")
 
 #export to file
-# kg.export("testExport3.owl", "ttl")  
+kg3 = ArangoSemantics("http://0.0.0.0:8529", "root", "password", "rdf", "arangoSemantic")
+kg3.export("testExport.owl", "ttl")  
