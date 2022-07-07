31ee70337470d11a373ff4bd6934599ef4bf2535
aMahanna <anthony.mahanna@gmail.com>
2022-07-07 10:59:40 -0400

    initial commit

--- tests/test.py
+++ tests/test.py
@@ -1,26 +1,22 @@
+from arango import ArangoClient
 from arango_rdf import ArangoRDF
-from tests import PROJECT_DIR
 
-# rdf import
-kg = ArangoRDF(
-    "http://localhost:8529",
-    username="root",
-    password="",
-    database="rdf",
-    graph="rdf_music",
-)
-kg.init_rdf_collections(bnode="Blank")
-kg.import_rdf(f"{PROJECT_DIR}/examples/data/music_schema.ttl", format="ttl")
-kg.import_rdf(f"{PROJECT_DIR}/examples/data/beatles.ttl", format="ttl")
-kg.import_rdf(f"{PROJECT_DIR}/examples/data/iao.owl")
+db = ArangoClient(hosts="http://localhost:8529").db("_system", username="root", password="")
 
-# ontology import
-kg2 = ArangoRDF("http://localhost:8529", "root", "", "ontologyImport", "ontology_iao")
-kg2.init_ontology_collections()
-kg2.import_ontology(f"{PROJECT_DIR}/examples/data/iao.owl")
+adb_rdf = ArangoRDF(db, graph="rdf_music")
 
-# export to file
-kg.export(f"{PROJECT_DIR}/examples/data/rdfExport.ttl", format="ttl")
+# RDF Import
+adb_rdf.init_rdf_collections(bnode="Blank")
+adb_rdf.import_rdf("./examples/data/music_schema.ttl", format="ttl")
+adb_rdf.import_rdf("./examples/data/beatles.ttl", format="ttl")
 
-# Re-import Export
-kg.import_rdf(f"{PROJECT_DIR}/examples/data/rdfExport.ttl", format="ttl")
+# RDF Export
+adb_rdf.export(f"./examples/data/rdfExport.ttl", format="ttl")
+
+# Re-import RDF Export
+adb_rdf.import_rdf(f"./examples/data/rdfExport.ttl", format="ttl")
+
+# Ontology Import
+adb_rdf_2 = ArangoRDF(db, graph="ontology_iao")
+adb_rdf_2.init_ontology_collections()
+adb_rdf_2.import_ontology("./examples/data/iao.owl")
